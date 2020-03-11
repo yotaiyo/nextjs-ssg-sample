a5a1d0cc04cfecf693c88cbf34c4f3785a20efff
yotaiyo <yota071218@gmail.com>
2020-03-11 14:33:01 +0900

    getStaticPropsを使用してみる

--- pages/showTodoTitle.js
+++ pages/showTodoTitle.js
@@ -4,10 +4,14 @@ function showTodoTitle({ todo }) {
   return <div>Todo Title: {todo.title}</div>
 }
 
-showTodoTitle.getInitialProps = async () => {
+export async function getStaticProps() {
   const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
   const todo = await res.json()
-  return { todo }
+  return {
+    props: {
+      todo
+    }
+  }
 }
 
 export default showTodoTitle
